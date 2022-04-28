1fb8cd821485f253bcc876f080dd23457cbfbc13
jasoncheng <shenjingwa444@qq.com>
2022-04-29 01:38:50 +0800

    NotebookList.vue 使用 element-ui

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import baseURLConfig from './config-baseURL'
-
+import {Message} from 'element-ui'
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 axios.defaults.baseURL = baseURLConfig.baseURL
 axios.defaults.withCredentials = true
@@ -23,11 +23,11 @@ export default function request(url, type = 'GET', data = {}) {
       if(res.status === 200) {
         resolve(res.data)
       }else {
-        console.error(res.data)
+        Message.error(res.data.msg)
         reject(res.data)
       }
-    }).catch(err=>{
-      console.error({msg: '网络异常'})
+    }).catch(res=>{
+      Message.error(res.data.msg)
       reject({msg: '网络异常'})
     })
   })
